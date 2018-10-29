d4bc0c1cd66c1704a7b3cb0ab934942c0867c352
raminarmanfar <ramin.armanfar@msg.group>
2018-10-29 14:55:00 +0100

    Logged user personal information under progress

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { AppMaterialsModule } from '../../app-material-module';
+
 import { ChangePasswordComponent } from './change-password/change-password.component';
 import { LoggedUserInfoComponent } from './logged-user-info/logged-user-info.component';
 import { DashRootComponent } from './dash-root/dash-root.component';
+import { UniqueValidatorDirective } from '../../utils/UniqueValidator.directive';
+import { PasswordValidatorDirective } from '../../utils/PasswordValidator.directive';
 
 @NgModule({
   imports: [
     CommonModule,
     DashboardRoutingModule,
+    FlexLayoutModule,
+    AppMaterialsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FontAwesomeModule,
   ],
   declarations: [
+    UniqueValidatorDirective,
+    PasswordValidatorDirective,
+
     ChangePasswordComponent,
     LoggedUserInfoComponent,
     DashRootComponent,
--- src/app/components/dashboard/logged-user-info/logged-user-info.component.ts
+++ src/app/components/dashboard/logged-user-info/logged-user-info.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-logged-user-info',
   templateUrl: './logged-user-info.component.html',
   styleUrls: ['./logged-user-info.component.scss']
 })
-export class LoggedUserInfoComponent implements OnInit {
+export class LoggedUserInfoComponent {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  submit(contactInfo: any) {
+    /*
+    this.contactService.addContactInfo(contactInfo).subscribe((result: ServiceResponse) => {
+      let popupData: any;
+      if (result.success) {
+        popupData = { title: 'Contact to the admin', message: result.message };
+      } else {
+        popupData = { title: 'Contact to the admin', message: result.message };
+      }
 
+      const dialogRef = this.dialog.open(DialogComponent, {
+        width: '400px',
+        data: popupData
+      });
+
+      // dialogRef.afterClosed().subscribe(dialogResult => {});
+    });
+    */
+  }
 }
