b8c58ea186c2e01540c83cc4016645f7a531c075
raminarmanfar <ramin.armanfar@msg.group>
2018-11-12 11:36:40 +0100

    Some updates done.

--- src/app/components/dashboard/for-admin/popup-add-update-user/popup-add-update-user.component.ts
+++ src/app/components/dashboard/for-admin/popup-add-update-user/popup-add-update-user.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-popup-add-update-user',
   templateUrl: './popup-add-update-user.component.html',
   styleUrls: ['./popup-add-update-user.component.scss']
 })
-export class PopupAddUpdateUserComponent implements OnInit {
+export class PopupAddUpdateUserComponent {
 
   constructor() { }
-
-  ngOnInit() {
-  }
-
 }
